b42a46cd83ccf70465c87b0136e70c22b0b16f7c
Sycorjannik <120626866+Sycorjannik@users.noreply.github.com>
2023-03-10 10:33:15 +0100

    Add files via upload

---
+++ initialize_disk.ps1
@@ -0,0 +1,21 @@
+$rawDisks = Get-Disk | Where-Object PartitionStyle -eq "RAW"
+
+if ($rawDisks.Count -eq 0) {
+    Write-Host "No offline disks found to initialize."
+    return
+}
+
+foreach ($disk in $rawDisks) {
+    if ($disk.IsOnline) {
+        Write-Host "Disk $($disk.Number) is already online."
+    }
+    else {
+        Initialize-Disk -Number $disk.Number -PartitionStyle MBR -PassThru
+        Write-Host "Disk $($disk.Number) initialized."
+    }
+    
+    New-Partition -DiskNumber $disk.Number -AssignDriveLetter -UseMaximumSize |
+        Format-Volume -FileSystem NTFS -Confirm:$false
+
+    Write-Host "Disk $($disk.Number) formatted with NTFS file system."
+}
